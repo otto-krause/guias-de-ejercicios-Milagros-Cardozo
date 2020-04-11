c0e2849301cad178ee718f3b0f1998fec7c8d7a9
Micaela Cardozo Munoz <micaelac323@alu.bue.edu.ar>
2020-04-11 20:59:08 -0300

    hice el 2

--- Guia 1/E2/Program.cs
+++ Guia 1/E2/Program.cs
@@ -2,11 +2,37 @@
 
 namespace E2
 {
-    class Program
+    class Matematicas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int produ = 0, num1= 0, num2 = 0, i = 0, elegir = 0, result = 0, resto = 0;
+            Console.WriteLine("Ingrese un numero: ");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero: ");
+            num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese 1 para multiplicar \n Ingrese 2 para dividir");
+            elegir = Int32.Parse(Console.ReadLine());
+            if (elegir == 1)
+            {
+                for (i = 0; i < num2; i++)
+                    {
+                        produ += num1;
+                    }
+            }
+            Console.WriteLine("El producto entre los dos numeros es: {0}", produ);
+            if (elegir == 2)
+            {
+                while(num1>num2)
+                {
+                    num1 -= num2;
+                    result += 1;
+                    resto = num1;
+                }
+            Console.WriteLine("La division es {0} y el resto {1} ", result, resto);
+            }
         }
+            
     }
 }
+
