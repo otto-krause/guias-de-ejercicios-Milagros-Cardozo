eb1c7aedc2ad2003e64e4ccc806199546ba3e2b8
Milagros-Cardozo <milagroscardozo200286@gmail.com>
2020-05-29 00:09:56 -0300

    hice el 5

--- Guia 4/E5/Program.cs
+++ Guia 4/E5/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
 
 namespace E5
 {
@@ -6,7 +7,61 @@ namespace E5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Serpensante serpensante=new Robot();
+            Humano humano=new Humano();
+            int opcion=1;
+            string opcion1,estudia,tema="";
+            do
+            {
+                Console.WriteLine("Ingrese la opcion que desea realizar\n"+
+                "1)Para que el humano estudie\n"+
+                "2)Para que el humano piense\n"+
+                "3)Para que el cyborg estudie\n"+
+                "4)Para que el cyborg piense\n"+
+                "5)Para que el robot estudie\n"+
+                "6)Para que el robot piense\n"+
+                "Ingrese cualquier otra letra para salir\n"); 
+                opcion1=Console.ReadLine();
+                switch (opcion1)
+                {
+                    case "1":
+                        serpensante=new Humano();
+                        Console.WriteLine("ingrese lo que estudio");
+                        estudia=Console.ReadLine();
+                        serpensante.estudiar(estudia);
+                        break;
+                    case "2":
+                        serpensante=new Humano();
+                        Console.WriteLine("Ingrese el tema a pensar:");
+                        tema=Console.ReadLine();
+                        Console.WriteLine("Puntos de inteligencia:"+serpensante.pensar(tema));
+                        break;
+                    case "3":
+                        serpensante=new Cyborg();
+                        Console.WriteLine("ingrese lo que estudio");
+                        estudia=Console.ReadLine();
+                        serpensante.estudiar(estudia);
+                        break;
+                    case "4":
+                        serpensante=new Cyborg();
+                        Console.WriteLine("Puntos de inteligencia:"+serpensante.pensar(tema));
+                        break;
+                    case "5":
+                        serpensante=new Robot();
+                        Console.WriteLine("ingrese lo que estudio");
+                        estudia=Console.ReadLine();
+                        serpensante.estudiar(estudia);
+                        break;
+                    case "6":
+                        serpensante=new Robot();
+                        Console.WriteLine("Puntos de inteligencia:"+serpensante.pensar(tema));
+                        break;
+                    default:
+                        opcion=2;
+                        break;
+                }
+            }
+            while(opcion==1);
         }
     }
 }
