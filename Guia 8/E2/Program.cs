53f18535b98e9b16b039f49560f6f2b4c24ec204
Milagros-Cardozo <milagroscardozo200286@gmail.com>
2020-11-20 20:52:14 -0300

    tampoco entendi

---
+++ Guia 8/E2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace E1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            peliculas = File.CreateText("prueba.txt");
+            Console.WriteLine("Ingrese los nombres de las pelis\n");
+            foreach (var i in fichero)
+            {
+                
+            }
+        }
+    }
+}
